b0980d5efa6385208157ce506450fe4cfbcd02d5
Lauro Moura <lauromoura@gmail.com>
2016-03-18 13:26:47 -0300

    Part 1 of Day 6

---
+++ day6.exs
@@ -0,0 +1,44 @@
+defmodule Day5 do
+  def process_file(file), do: process_file(file, Map.new())
+
+  defp process_file(file, current) do
+    row = IO.read(file, :line)
+    if (row != :eof) do
+      parsed_row = parse_row(String.strip(row))
+      IO.inspect(parsed_row)
+      process_file(file, process_row(parsed_row, current))
+    else
+      IO.inspect(Map.values(current))
+      Enum.count(Enum.filter(Map.values(current), &(&1)))
+    end
+  end
+
+  defp parse_row(row) do
+    regex = ~r/(.*) (\d+),(\d+) through (\d+),(\d+)/
+    [_row, ops | coords] = Regex.run(regex, row)
+    operation = String.to_atom(ops)
+    coords = Enum.map(coords, &String.to_integer(&1))
+    [operation | coords]
+  end
+
+  defp process_row([operation, x0, y0, x1, y1], current) do
+    func = case operation do
+      :toggle -> fn x -> not x end
+      :"turn on" -> fn _x -> true end
+      :"turn off" -> fn _x -> false end
+    end
+    coords = for x <- x0..x1, y <- y0..y1 do
+      {x, y}
+    end
+    Enum.reduce coords, current, fn coord, data ->
+      data = Map.put_new(data, coord, false)
+      Map.update!(data, coord, func)
+    end
+  end
+end
+
+
+file = File.open!("day6.txt", [:read])
+IO.puts(Day5.process_file(file))
+#file = File.open!("day5.txt", [:read])
+#IO.puts(Day5.process_file(file, :parttwo))
