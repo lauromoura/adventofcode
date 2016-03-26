50523c41d9a1c21fb8f697b0e08e8a76e52d103f
Lauro Moura <lauromoura@gmail.com>
2016-03-26 20:22:11 -0300

    Refactor to allow part two method.

--- day8/lib/counter.ex
+++ day8/lib/counter.ex
@@ -1,16 +1,16 @@
 defmodule Counter do
-  def count(str) do
+  def count_decode(str) do
     code_size = String.length(str)
     {parsed, _} = Code.eval_string(str)
     {code_size, String.length(parsed)}
   end
 
-  def count_file(filename) do
+  def count_file(filename, function) do
     stream = File.stream!(filename, [:read])
 
     Enum.reduce(stream, {0, 0},
       fn str, {code, real} ->
-        {row_code, row_real} = count(String.strip str)
+        {row_code, row_real} = function.(String.strip str)
         {row_code + code, row_real + real}
       end)
   end
--- day8/test/counter_test.exs
+++ day8/test/counter_test.exs
@@ -9,21 +9,21 @@ defmodule CounterTest do
 
   test "Empty string" do
     str = load_file("test/empty.txt")
-    assert {2, 0} == Counter.count(str)
+    assert {2, 0} == Counter.count_decode(str)
   end
   test "Simple string" do
     str = load_file("test/simple.txt")
-    assert {5, 3} == Counter.count(str)
+    assert {5, 3} == Counter.count_decode(str)
   end
   test "Escaped quote" do
-    assert {10, 7} == Counter.count_file("test/escaped_quote.txt")
+    assert {10, 7} == Counter.count_file("test/escaped_quote.txt", &Counter.count_decode/1)
   end
   test "Escaped char" do
-    assert {6, 1} == Counter.count_file("test/escaped_char.txt")
+    assert {6, 1} == Counter.count_file("test/escaped_char.txt", &Counter.count_decode/1)
   end
 
   test "Advent of code part 1" do
-    {code, real} =  Counter.count_file("data/day8.txt")
+    {code, real} =  Counter.count_file("data/day8.txt", &Counter.count_decode/1)
     assert code - real == 1333
   end
 end
