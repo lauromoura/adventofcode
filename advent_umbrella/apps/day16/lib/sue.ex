e1200eda14bf47dbca4eee9a8b3d66e40cfb31e0
Lauro Moura <lauromoura@gmail.com>
2016-04-27 23:00:26 -0300

    Added part 2 of day16

--- advent_umbrella/apps/day16/lib/sue.ex
+++ advent_umbrella/apps/day16/lib/sue.ex
@@ -1,9 +1,35 @@
 defmodule Sue do
 
-  def find(sues, hint) do
+  def unretroencabulate(key, hint, candidate) do
+    cond do
+      key in [:cats, :trees] ->
+        if candidate > hint do
+          hint
+        else
+          -hint
+        end
+      key in [:pomeranians, :goldfish] ->
+        if candidate < hint do
+          hint
+        else
+          -hint
+        end
+      true ->
+        candidate
+    end
+  end
+
+  def find(sues, hint, retroencabulated \\ :normal) do
     sues
     |> Enum.find(fn {key, value} -> 
-          MapSet.subset?(value, hint)
+          updated = Map.merge(hint, value, fn key, v1, v2 ->
+            if retroencabulated == :retroencabulated do
+              unretroencabulate(key, v1, v2)
+            else
+              v2
+            end
+          end)
+          Map.equal?(hint, updated)
         end)
     |> elem(0)
   end
@@ -11,11 +37,11 @@ defmodule Sue do
   def parse(row) do
     regex = ~r/Sue ([0-9]+): (\w+): ([0-9]+), (\w+): ([0-9]+), (\w+): ([0-9]+)/
     [_, number, feature1, value1, feature2, value2, feature3, value3] = Regex.run(regex, String.strip(row))
-    sue = MapSet.new(%{
+    sue = %{
       (String.to_atom feature1) => (String.to_integer value1),
       (String.to_atom feature2) => (String.to_integer value2),
       (String.to_atom feature3) => (String.to_integer value3),
-    })
+    }
     {(String.to_integer number), sue}
   end
 end
