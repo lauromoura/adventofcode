a2853030375a83d860a49412428f3694fe00037f
Lauro Moura <lauromoura@gmail.com>
2018-12-03 10:43:13 -0300

    Added day 1 of 2018

---
+++ advent2018/lib/day01.ex
@@ -0,0 +1,26 @@
+defmodule Day01 do
+  def frequency(changes) do
+    changes
+    |> parse_changes
+    |> Enum.sum
+  end
+
+  def repeated(changes) do
+    changes
+    |> parse_changes
+    |> Stream.cycle
+    |> Enum.reduce_while({0, MapSet.new([0])}, fn f, {s, freqs} -> 
+      new = f + s
+      if MapSet.member?(freqs, new) do
+        {:halt, new}
+      else
+        {:cont, {new, MapSet.put(freqs, new)}}
+      end
+    end)
+  end
+
+  defp parse_changes(changes) do
+    changes
+    |> Enum.map(&String.to_integer/1)
+  end
+end
---
+++ advent2018/test/day1_test.exs
@@ -0,0 +1,31 @@
+defmodule Day01Test do
+  use ExUnit.Case
+
+  test "basic tests" do
+    assert Day01.frequency(["+1", "-2", "+3", "+1"]) == 3
+    assert Day01.frequency(["+1", "+1", "+1"]) == 3
+    assert Day01.frequency(["+1", "+1", "-2"]) == 0
+    assert Day01.frequency(["-1", "-2", "-3"]) == -6
+  end
+
+  test "question 1" do
+    freq = "test/day01_1_input.txt"
+           |> File.stream!([], :line)
+           |> Stream.map(&String.trim/1)
+           |> Day01.frequency
+    assert freq == 518
+  end
+
+  test "basic part two" do
+    frequencies = ["+1", "-2", "+3", "+1"]
+    assert Day01.repeated(frequencies) == 2
+  end
+
+  test "question 2" do
+    freq = "test/day01_1_input.txt"
+           |> File.stream!([], :line)
+           |> Stream.map(&String.trim/1)
+           |> Day01.repeated
+    assert freq == 72889
+  end
+end
