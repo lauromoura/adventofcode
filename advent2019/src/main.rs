86fd7cf09069d57c9ab0300a07345b8c7962e0e9
Lauro Moura <lauromoura@expertisesolutions.com.br>
2019-12-05 14:39:17 -0300

    Starting 2019 late. This year, Rust!

---
+++ advent2019/src/main.rs
@@ -0,0 +1,55 @@
+mod day_01 {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    pub fn first() {
+        let filename = "day01-01.txt";
+        let file = File::open(filename).unwrap();
+        let reader = BufReader::new(file);
+
+        let fuel_for_modules: i32 = reader
+            .lines()
+            .map(|s| s.unwrap().parse::<i32>().unwrap())
+            .map(|i| weigth(i))
+            .sum();
+        println!("Day 1 - 1: {}", fuel_for_modules);
+    }
+
+    pub fn second() {
+        let filename = "day01-01.txt";
+        let file = File::open(filename).unwrap();
+        let reader = BufReader::new(file);
+
+        let fuel_for_modules: i32 = reader
+            .lines()
+            .map(|s| s.unwrap().parse::<i32>().unwrap())
+            .map(|i| total_fuel(i))
+            .sum();
+        println!("Day 1 - 2: {}", fuel_for_modules);
+    }
+
+    fn total_fuel(mass: i32) -> i32 {
+        let initial_fuel = weigth(mass);
+
+        let mut total = initial_fuel;
+        let mut current_fuel = total;
+
+        while current_fuel > 0 {
+            let new_fuel = weigth(current_fuel);
+            if new_fuel > 0 {
+                total += new_fuel
+            }
+            current_fuel = new_fuel;
+        }
+        total
+    }
+
+    fn weigth(mass: i32) -> i32 {
+        mass / 3 - 2
+    }
+}
+
+fn main() {
+    day_01::first();
+    day_01::second();
+}
